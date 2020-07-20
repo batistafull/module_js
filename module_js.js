9f5ae81b2c538c9f3a793d86657372bfc949fbf9
Alberto Batista <albertomimi16@gmail.com>
2020-07-20 16:32:22 -0400

    removiendo el windows onload

--- module_js.js
+++ module_js.js
@@ -1,8 +1,6 @@
-window.onload = () => {
-    const _mjs = (_p, _c) => {
-        let _s = document.createElement('script');
-        _s.src = _p;
-        document.head.append(_s);
-        _s.onload = () => _c();
-    }
+const _mjs = (_p, _c) => {
+    let _s = document.createElement('script');
+    _s.src = _p;
+    document.head.append(_s);
+    _s.onload = () => _c();
 }
